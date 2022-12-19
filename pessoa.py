a4843c55bbe798e6077bd5c07db9e4cc94eba4f7
DudaC0d3r <118295787+DudaC0d3r@users.noreply.github.com>
2022-12-19 19:17:26 -0300

    Add files via upload
    created enemy clas

--- pessoa.py
+++ pessoa.py
@@ -1,12 +1,34 @@
+import random
+
 from pokemon import *
 
+NOMES = [
+        'joao', 'isabela', 'lorena', 'francisco', 'ricardo',
+        'patricia', 'marcelo', 'gustavo', 'geronimo'
+]
+
+POKEMONS = [
+   PokemonFogo('charmander'),
+   PokemonPlanta('bubasauro'),
+   PokemonSombrio('zubat'),
+   PokemonPsiquico('psyduck'),
+   PokemonVeneno('eikans'),
+   PokemonInseto('beedril'),
+   PokemonSombrio('arboft'),
+   PokemonEletrico('pikachu'),
+   PokemonPsiquico('abra'),
+   PokemonSombrio('mimikyu'),
+]
+
+
 class Pessoa:
+
     def __init__(self, nome=None, pokemons=[]):
         if nome:
             self.nome = nome
 
         else:
-            self.nome = 'Anonimo'
+            self.nome = random.choice(NOMES)
 
         self.pokemons = pokemons
 
@@ -15,10 +37,12 @@ class Pessoa:
 
     def mostrar_pokemons(self):
         if self.pokemons:
+            print('Pokemons de {}:'.format(self))
             for pokemon in self.pokemons:
                 print(pokemon)
         else:
-            print('{} nao tem nenhum pokemon'.format(self))
+           print('{} nao tem nenhum pokemon'.format(self))
+
 
 class Player(Pessoa):
     tipo = 'player'
@@ -27,17 +51,19 @@ class Player(Pessoa):
         self.pokemons.append(pokemon)
         print('{} capturou {}'.format(self, pokemon))
 
+
 class Inimigo(Pessoa):
-    tipo = 'Inimigo'
+    tipo = 'inimigo'
+    
+    def __init__(self, nome=None, pokemons=[]):
+        if not pokemons:
+            for i in range(random.randint(1, 6)):
+                pokemons.append(random.choice(POKEMONS))
 
-meu_pokemon = PokemonSolar('Solgaleio', '100000')
-meu_pokemon2 = PokemonLunar('Lunala', '100000')
+        super().__init__(nome=nome, pokemons=pokemons)
 
-eu = Player(nome='Yumi', pokemons=[meu_pokemon, meu_pokemon2])
+inimigo = Inimigo()
+print(inimigo.tipo)
+inimigo.mostrar_pokemons()
 
-print(eu)
-eu.mostrar_pokemons()
 
-pokemon_selvagem = PokemonLunar('Lunala')
-
-eu.capturar(pokemon_selvagem)
--- pokemon.py
+++ pokemon.py
@@ -1,5 +1,7 @@
+import random
+
 class Pokemon:
-    def __init__(self, especie, level=1, nome=None):
+    def __init__(self, especie, level=random.randint(1, 100), nome=None):
         self.especie = especie
         self.level = level
 
@@ -27,9 +29,75 @@ class PokemonLunar(Pokemon):
     def atacar(self, pokemon):
         print('{} lançou um ataque lunar em {}'.format(self, pokemon))
 
+class PokemonFogo(Pokemon):
+    tipo = 'fogo'
+
+    def atacar(self, pokemon):
+        print('{} lençou um ataque de fogo em {}'.format(self, pokemon))
+
+class PokemonEletrico(Pokemon):
+    tipo = 'eletrico'
+
+    def atacar(self, pokemon):
+        print('{} lançou um ataque de fogo em {}'.format(self, pokemon))
+
+class PokemonSombrio(Pokemon):
+    tipo = 'sombrio'
+
+    def atacar(self, pokemon):
+        print('{} lençou um ataque sombrio em {}'.format(self, pokemon))
+
+class PokemonAgua(Pokemon):
+    tipo = 'agua'
+
+    def atacar(self, pokemon):
+        print('{} lançou um ataque de agua em {}'.format(self, pokemon))
+
+class PokemonFada(Pokemon):
+    tipo = 'fada'
+
+    def atacar(self, pokemon):
+        print('{} lançou um ataque magico em {}'.format(self, pokemon))
+
+
+class PokemonPlanta(Pokemon):
+    tipo = 'planta'
+
+    def atacar(self, pokemon):
+        print('{} lançou um ataque planta em {}'.format(self, pokemon))
+
+
+class PokemonPsiquico(Pokemon):
+    tipo = 'psiquico'
+
+    def atacar(self, pokemon):
+        print('{} lançou um ataque psiquico em {}'.format(self, pokemon))
+
+
+class PokemonLutador(Pokemon):
+    tipo = 'lutador'
+
+    def atacar(self, pokemon):
+        print('{} deu um golpe em {}'.format(self, pokemon))
+
+
+class PokemonVeneno(Pokemon):
+    tipo = 'veneno'
+
+    def atacar(self, pokemon):
+        print('{} lançou um ataque de veneno em {}'.format(self, pokemon))
+
+
+class PokemonInseto(Pokemon):
+    tipo = 'inseto'
+
+    def atacar(self, pokemon):
+        print('{} lançou um ataque de inseto em {}'.format(self, pokemon))
+
+
 meu_pokemon = PokemonSolar(level=1000000, especie='solgaleio')
 
-pokemon_amigo = PokemonLunar('lunala', '10000')
+pokemon_amigo = PokemonLunar('lunala')
 
-meu_pokemon.atacar(pokemon_amigo)
-pokemon_amigo.atacar(meu_pokemon)
+
+
