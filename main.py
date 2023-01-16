2788b10c19bcb6efe587359d3a27028fa2e19caf
DudaC0d3r <118295787+DudaC0d3r@users.noreply.github.com>
2023-01-16 19:38:05 -0300

    Add files via upload
    added main menu

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pessoa
 from pokemon import *
 
 from pessoa import *
@@ -32,12 +33,53 @@ def escolher_pokemon_inicial(player):
         else:
             print('opção inexistente, por favor escolha 1 2 ou 3 para escolher um pokemon')
 
-jogador = Player(nome='Yumi')
-#escolher_pokemon_inicial(jogador)
-jogador.capturar(PokemonSombrio('Umbreon', level=1))
+if __name__ == '__main__':
+    print('----------------------------------')
+    print('bem-vindo ao game pokemon RPG de terminal')
+    print('----------------------------------')
 
-inimigo1 = Inimigo(nome='Gary', pokemons=[PokemonEletrico('Pikachu', level=1)])
+    nome = input('ola, qual seu nome?: ')
+    jogador = Player(nome)
+    print('ola {}, esse é um mundo abitado por pokemons, apartir de agora sua missao é se tornar um mestre pokemon '.format(jogador))
+    print('capture o maximo de pokemons que conseguir e lute contra os inimigos')
+    jogador.mostrar_dinheiro()
 
-jogador.batalhar(inimigo1)
+    if jogador.pokemons:
+        print('já vi que vc tem alguns pokemons')
+        jogador.mostrar_pokemons()
+    else:
+        print('vc nn tem nenhum pokemon, portanto precisa escolher um')
+        escolher_pokemon_inicial(jogador)
 
+    print('pronto, agora que vc ja possui um pokemon, enfrente seu arqui-rival Gary ')
+    gary = Inimigo(nome='Gary', pokemons=[PokemonPlanta('bubassauro', level=1)])
+    jogador.batalhar(gary)
 
+    while True:
+        print('----------------------------------')
+        print('o que deseja fazer?')
+        print('1- exploxrar pelo mundo')
+        print('2- lutar com um inimigo')
+        print('0- sair do jogo')
+        escolha = input('sua escolha:')
+
+        if escolha == '0':
+            print('fechando o jogo')
+            break
+        elif escolha == '1':
+            jogador.explorar()
+        elif escolha == '2':
+            inimigo_aleatorio = Inimigo()
+            jogador.batalhar(inimigo_aleatorio)
+        else:
+            print('escolha invalida')
+
+    #escolher_pokemon_inicial(jogador)
+    #jogador.capturar(PokemonSombrio('Umbreon', level=1))
+
+    #inimigo1 =
+    #jogador.batalhar(inimigo1)
+
+    #jogador.explorar()
+
+    #jogador.mostrar_pokemons()
